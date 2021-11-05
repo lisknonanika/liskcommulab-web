c5f6fa70a40a1779fee3bfa322871c5571842e6a
lisknonanika <lisknonanika@gmail.com>
2021-11-05 09:54:09 +0900

    upd

--- src/components/Event.js
+++ src/components/Event.js
@@ -21,13 +21,15 @@ class Event extends React.Component {
           <div className="content-area">
             <div className="title">{this.props.translation('Lisk 公式')}</div>
             <div className="content">
+
               <div style={{borderBottom: "1px dotted #404041"}}>
                 <div className="event-title">AmpliFire</div>
                 <div className="event-detail">
-                  <div>{this.props.translation('日時')}： 2021/12/3 AM2:00～AM7:00 (JST)</div>
-                  <div>{this.props.translation('詳細')}：&nbsp;
+                  <div>{this.props.translation('日時')}： 2021/12/3 2:00～7:00 (JST)</div>
+                  <div>{this.props.translation('アナウンス')}：&nbsp;
                     <a href="https://lisk.com/blog/announcement/amplifire-event-lisk" target="_new">lisk.com</a>
-                    &nbsp;/&nbsp;
+                  </div>
+                  <div>{this.props.translation('詳細')}：&nbsp;
                     <a href="https://www.youtube.com/watch?v=31XPRrrBBX4" target="_new">youtube</a>
                     &nbsp;/&nbsp;
                     <a href="https://www.eventbrite.com/e/amplifire-tickets-201633179267" target="_new">eventbrite</a>
@@ -37,33 +39,49 @@ class Event extends React.Component {
               </div>
 
               <div style={{borderBottom: "1px dotted #404041"}}>
-                <div className="event-title">Ask Max Anything</div>
-                <div className="event-detail">
-                  <div>{this.props.translation('日時')}： 2021/11/11 AM1:00〜 (JST)</div>
-                  <div>{this.props.translation('詳細')}：&nbsp;
-                    <a href="https://twitter.com/maxkordek/status/1455487974527881225" target="_new">twitter</a>
-                    &nbsp;/&nbsp;
-                    <a href="https://www.youtube.com/watch?v=P13z_u5ahoc" target="_new">youtube</a>
-                  </div>
-                  <div>{this.props.translation('タグ')}： AMA, {this.props.translation('オンライン')}</div>
-                </div>
-              </div>
-
-              <div>
                 <div className="event-title">HackOnLisk2</div>
                 <div className="event-detail">
-                  <div>{this.props.translation('日時')}： 2021/9/11 AM1:15～2021/12/3 AM2:00 (JST)</div>
-                  <div>{this.props.translation('詳細')}：&nbsp;
+                  <div>{this.props.translation('日時')}： 2021/9/11 1:15～2021/12/3 2:00 (JST)</div>
+                  <div>{this.props.translation('アナウンス')}：&nbsp;
                     <a href="https://lisk.com/blog/announcement/announcing-hackonlisk2" target="_new">lisk.com</a>
-                    &nbsp;/&nbsp;
+                  </div>
+                  <div>{this.props.translation('詳細')}：&nbsp;
                     <a href="https://hackonlisk2.devpost.com/" target="_new">devpost</a>
                   </div>
                   <div>{this.props.translation('タグ')}：{this.props.translation('ハッカソン')}, {this.props.translation('オンライン')}</div>
                 </div>
               </div>
+
+              <div>
+                <div className="event-title">Ask Max Anything</div>
+                <div className="event-detail">
+                  <div>{this.props.translation('日時')}： 2021/11/11 1:00〜 (JST)</div>
+                  <div>{this.props.translation('アナウンス')}：&nbsp;
+                    <a href="https://twitter.com/maxkordek/status/1455487974527881225" target="_new">twitter</a>
+                  </div>
+                  <div>{this.props.translation('詳細')}：&nbsp;
+                    <a href="https://www.youtube.com/watch?v=P13z_u5ahoc" target="_new">youtube</a>
+                  </div>
+                  <div>{this.props.translation('タグ')}： {this.props.translation('定期')}, AMA, {this.props.translation('オンライン')}</div>
+                </div>
+              </div>
+
+              <div>
+                <div className="event-title">Lisk Army Contest</div>
+                <div className="event-detail">
+                  <div>{this.props.translation('日時')}： 2021/12/1 15:00〜 (JST)</div>
+                  <div>{this.props.translation('アナウンス')}：&nbsp;
+                    <a href="https://twitter.com/LiskHQ/status/1456283209663123463" target="_new">twitter</a>
+                  </div>
+                  <div>{this.props.translation('詳細')}：&nbsp;
+                    <a href="https://sweepwidget.com/view/38437-g7p8qesk" target="_new">sweepwidget</a>
+                  </div>
+                  <div>{this.props.translation('タグ')}： {this.props.translation('恒常')}, {this.props.translation('オンライン')}</div>
+                </div>
+              </div>
+
             </div>
           </div>
-          
         </div>
       </div>
     );
