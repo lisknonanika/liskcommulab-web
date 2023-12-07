1e5372df06bbf12cf79b38fcdb3bf95471f9bb84
lisknonanika <lisknonanika@gmail.com>
2023-12-07 14:13:04 +0900

    upd

--- public/js/common.js
+++ public/js/common.js
@@ -1,10 +1,48 @@
 const SERVICE_URL = "mainnet-service.lisk.com";
 const VALIDATOR_ADDRESS = "lsk5tyhc6tw76ybwwzt9vcefy3gryjvprkcj329tw";
+const DAILY_GENERATE_BLOCK = 24 * 60 * 60 / 10 / 103;
+
+const getCurrentHeight = async() => {
+    const ret = await fetch(`https://${SERVICE_URL}/api/v3/blocks?limit=1`);
+    const json = await ret.json();
+    return json.data[0].height;
+}
 
 const getMyAccount = async () => {
     const response = await fetch(`https://${SERVICE_URL}/api/v3/pos/validators?address=${VALIDATOR_ADDRESS}&limit=1`);
     const json = await response.json();
     return json.data[0];
+}
+
+const getMyReward = async() => {
+    const ret = await fetch(`https://${SERVICE_URL}/api/v3/blocks?generatorAddress=${VALIDATOR_ADDRESS}&limit=1`);
+    const json = await ret.json();
+    const data = json.data[0];
+    blockReward = +data.reward / 100000000;
+}
+
+const getValidators = async() => {
+    const ret = await fetch(`https://${SERVICE_URL}/api/v3/pos/validators?sort=rank:asc&limit=101`);
+    const json = await ret.json();
+    return json.data;
+}
+
+const getBFTParameters = async(height) => {
+    const body = {
+        "endpoint": "consensus_getBFTParameters",
+        "params": {
+            "height": height
+        }
+    }
+    const ret = await fetch(`https://${SERVICE_URL}/api/v3/invoke`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body)
+    });
+    const json = await ret.json();
+    return json.data;
 }
 
 const toggleContent = (node) => {
