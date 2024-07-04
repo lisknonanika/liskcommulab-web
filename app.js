83ac1367be6a06d9cd444e3d7ded7f08a592d4ec
lisknonanika <lisknonanika@gmail.com>
2024-07-04 16:11:43 +0900

    del: cvs-download

--- app.js
+++ app.js
@@ -32,15 +32,6 @@ app.get("/network-monitor", (req, res, next) => {
     });
 });
 
-app.get("/csv-download", (req, res, next) => {
-    res.render("template/index.ejs",
-    {
-        data: {
-            type: "csv-download"
-        }
-    });
-});
-
 app.listen(port, ()=> {
     console.log(`[start]commulab web(http://127.0.0.1:${port})`);
 });
--- public/js/common.js
+++ public/js/common.js
@@ -1,19 +1,3 @@
-const SERVICE_URL = "service.lisk.com";
-const VALIDATOR_ADDRESS = "lsk5tyhc6tw76ybwwzt9vcefy3gryjvprkcj329tw";
-
-const getMyReward = async() => {
-    const ret = await fetch(`https://${SERVICE_URL}/api/v3/blocks?generatorAddress=${VALIDATOR_ADDRESS}&limit=1`);
-    const json = await ret.json();
-    const data = json.data[0];
-    blockReward = +data.reward / 100000000;
-}
-
-const getYMD = (days) => {
-    const d = new Date();
-    if (days !== undefined) d.setDate(d.getDate() + days);
-    return `${d.getFullYear()}-${("00" + (d.getMonth() + 1)).slice(-2)}-${("00" + d.getDate()).slice(-2)}`;
-}
-
 const toggleContent = (node) => {
     const content = node.querySelector(".content");
     const icon = node.querySelector(".open-close i");
